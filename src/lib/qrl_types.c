369c7470827d83e85cb8934743a091bc81bb81d3
Scott Donald <scottdonald@protonmail.ch>
2018-12-19 11:13:07 +1100

    Fix typo

--- src/lib/qrl_types.c
+++ src/lib/qrl_types.c
@@ -47,7 +47,7 @@ int16_t get_qrltx_size(const qrltx_t *tx_p) {
             break;
         }
 #endif
-#ifdef TXTOKEN_ENABLED
+#ifdef SLAVE_ENABLED
         case QRLTX_SLAVE: {
             if (tx_p->subitem_count > QRLTX_SUBITEM_MAX) {
                 return -1;
